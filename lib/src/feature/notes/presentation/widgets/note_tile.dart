24647e803703d792e919707cd65d923ffd1f5865
Kayw Chang <kchangrk@gmail.com>
2023-12-04 17:04:17 +0600

    note modified

--- lib/src/feature/notes/presentation/widgets/note_tile.dart
+++ lib/src/feature/notes/presentation/widgets/note_tile.dart
@@ -1,98 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../../../../core/constants/common_imports.dart';
 
-class NoteTile extends StatefulWidget {
+class NoteTile extends StatelessWidget with AppTheme {
   final String noteContent;
   final String title;
   final String timestamp;
-  final bool? hasDivider;
   final VoidCallback onPressed;
-
   const NoteTile(
-      {Key? key,
-        required this.noteContent,
-        required this.title,
-        required this.timestamp,
-        this.hasDivider = false, required this.onPressed})
-      : super(key: key);
+      {super.key,
+      required this.noteContent,
+      required this.title,
+      required this.timestamp,
+      required this.onPressed});
 
-  @override
-  _NoteTileState createState() => _NoteTileState();
-}
-
-class _NoteTileState extends State<NoteTile> with AppTheme {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: widget.onPressed,
-      child: SizedBox(width: 1.sw,
-        child: Column(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: size.h16),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(
+          width: size.w1,
+          color: clr.boxStrokeColor,
+        ))),
+        child: Row(
           children: [
-            SizedBox(
-              height: size.h12,
-            ),
-            Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(size.r8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(size.r10)),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: SizedBox(
-                    width: 35.w,
-                    child: Text(
-                      widget.noteContent,
-                      maxLines: 2,
-                      style: TextStyle(
-                        fontFamily: StringData.fontFamilyPoppins,
-                        fontWeight: FontWeight.w500,
-                        fontSize: size.textXXSmall,
-                        color: clr.blackColor,
-                      ),
-                    ),
-                  ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(size.r8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(size.r10)),
+                  border: Border.all(color: Colors.grey),
                 ),
-                const SizedBox(
-                  width: 16.0,
+                child: Text(
+                  noteContent,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontFamily: StringData.fontFamilyPoppins,
+                    fontWeight: FontWeight.w500,
+                    fontSize: size.textXXSmall,
+                    color: clr.blackColor,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.title,
-                      style: TextStyle(
-                        color: clr.blackColor,
-                        fontWeight: FontWeight.w400,
-                        fontSize: size.textSmall,
-                        fontFamily: StringData.fontFamilyPoppins,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 4.0,
-                    ),
-                    Text(
-                      widget.timestamp,
-                      style: TextStyle(
-                        color: clr.placeHolderTextColorGray,
-                        fontWeight: FontWeight.w400,
-                        fontSize: size.textXXSmall,
-                        fontFamily: StringData.fontFamilyPoppins,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              ),
             ),
-            SizedBox(
-              height: size.h12,
-            ),
-            widget.hasDivider == true
-                ? Divider(
-              color: clr.boxStrokeColor,
+            SizedBox(width: size.w16),
+            Expanded(
+              flex: 5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: clr.blackColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: size.textSmall,
+                      fontFamily: StringData.fontFamilyPoppins,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: size.h4),
+                  Text(
+                    timestamp,
+                    style: TextStyle(
+                      color: clr.placeHolderTextColorGray,
+                      fontWeight: FontWeight.w400,
+                      fontSize: size.textXXSmall,
+                      fontFamily: StringData.fontFamilyPoppins,
+                    ),
+                  ),
+                ],
+              ),
             )
-                : const SizedBox()
           ],
         ),
       ),
