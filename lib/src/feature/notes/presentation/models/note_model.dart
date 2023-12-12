1ca0859e6dafa0e43d15da76f636aa90645f1bc5
abir <abirrahman959@gmail.com>
2023-12-12 12:11:53 +0600

    Note v1

--- lib/src/feature/notes/presentation/models/note_model.dart
+++ lib/src/feature/notes/presentation/models/note_model.dart
@@ -2,12 +2,14 @@ class NoteModel {
   int? id;
   String? title;
   String? time;
+  String? reference;
   List<dynamic>? description;
 
   NoteModel({
      this.title,
      this.time,
      this.description,
-    this.id
+    this.id,
+    this.reference
   });
 }
